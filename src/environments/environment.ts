032c657ea99b7e5a67de42af467b3d713b80d158
Francis Oliveira <fnolivei@outlook.com>
2018-02-11 01:02:45 -0200

    adequar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'tc-rest:9090',
-    apiUrlAuth: 'tc-as:9998',
-    apiUrlRest: 'tc-rest:9090'
+    apiUrl: 'http://tc-rest:9090',
+    apiUrlAuth: 'http://tc-as:9998',
+    apiUrlRest: 'http://tc-rest:9090'
     
 };
