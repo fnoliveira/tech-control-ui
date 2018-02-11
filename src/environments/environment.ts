a435be966bd85957badd6ba9b11e87f70a0873c1
Francis Oliveira <fnolivei@outlook.com>
2018-02-10 22:03:53 -0200

    adequar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'tc-rest:9090',
-    apiUrlAuth: 'tc-as:9998',
-    apiUrlRest: 'tc-rest:9090'
+    apiUrl: 'https://tec-control.herokuapp.com',
+    apiUrlAuth: 'http://localhost:9998',
+    apiUrlRest: 'http://localhost:9090'
     
 };
