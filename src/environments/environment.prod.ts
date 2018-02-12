3f445c2c0c6f2f879852accfb052652e8d56c927
Francis Oliveira <fnolivei@outlook.com>
2018-02-12 10:43:38 -0200

    cors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://prod-network:9090',
-    apiUrlAuth: 'http://prod-network:9998',
-    apiUrlRest: 'http://prod-network:9090'
+    apiUrl: 'tc-rest:9090',
+    apiUrlAuth: 'tc-as:9998',
+    apiUrlRest: 'tc-rest:9090'
 };
